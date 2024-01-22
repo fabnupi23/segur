9ab8700d0f95299887c28ea0a7f9155e8349a2f2
Fabian_Nuñez <31360808+fabnupi23@users.noreply.github.com>
2024-01-22 17:27:30 -0500

    Valor Nulo

--- bin/segur.dart
+++ bin/segur.dart
@@ -2,7 +2,13 @@ import 'package:segur/segur.dart' as segur;
 
 void main(List<String> arguments) {
   String? name = 'Fabian';
-  name = null;
+  if (10 < 5) {
+    name = null;
+  }
+
+  //name = null;
+
+  name.length;
 
   if (name != null) {
     print(name.length);
